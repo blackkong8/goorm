52ff3fb50aa72706d2ba0771495e4146e06ae670
박예준 <blackkong8@gmail.com>
2018-10-13 01:38:24 +0000

    test4

--- scr/Ide.c
+++ scr/Ide.c
@@ -35,7 +35,7 @@ void LetterCapitalizer(char input[]){
 }
 
 int main(){
-    char input[]={"i ---  go to school"};
+    char input[]={"i ---  go to ----- school"};
     LetterCapitalizer(input);
     return 0;
 }
